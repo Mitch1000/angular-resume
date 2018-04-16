bb2bd491dcac7b1068590675fd2e1075f35d8da8
Mitch1000 <mitchell@fluidmedia.ca>
2018-04-16 10:17:04 -0400

    updates post to email server to fit express style server

--- src/components/contact/contact.component.js
+++ src/components/contact/contact.component.js
@@ -11,42 +11,61 @@
 
     this.$onInit = () => {
       this.sendMail = sendMail;
-  
-    }
+      this.showModal = false;
 
+      this.closeModal = () => {
+        this.showModal = !this.showModal;
+      };
+    }
 
     function sendMail() {
       let data = {
         "name": this.name,
+        "companyName": this.companyName,
         "email": this.email,
         "subject": this.subject,
         "content": this.content
       }
 
-      this.name = '';
-      this.email= '';
-      this.subject= '';
-      this.content= '';
-
       data = JSON.stringify(data);
-      
-      var http = new XMLHttpRequest();
-      var params = data;
-      http.open("POST", "http://159.203.26.129:3000", true);
 
-      http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-      http.onreadystatechange = function() {
-        console.log('onreadystatechange');
-        if (http.readyState == 4 && http.status == 200) {
-          console.log(http.responseText);
+      const showModal = (success) => {
+        let modalData = {}
+        if (success) {
+          modalData.modalTitle = 'Email Sent';
+          modalData.modalFail = false;
+          modalData.modalContent = '';
+        } else {
+          modalData.modalTitle = 'Failed to Send Email Due to Server Error.';
+          modalData.modalFail = true;
+          modalData.modalContent = 'Please try again later.';
         }
-        else {
-          console.log('readyState=' + http.readyState + ', status: ' + http.status);
-        }
-      }
 
-      http.send(params);
+        this.modalTitle = modalData.modalTitle;
+        this.modalFail = modalData.modalFail;
+        this.modalContent = modalData.modalContent;
+        this.showModal = !this.showModal;
+      };
+
+      $http.post('http://159.89.114.62:8080/send_email', data, {headers : {"Content-Type" : "application/json"}}).then((response) => {
+        const clearEmailForm = () => {
+          this.companyName = '';
+          this.name = '';
+          this.email= '';
+          this.subject= '';
+          this.content= '';
+        };
+
+        clearEmailForm()
+        showModal(true)
+ 
+        console.log(response);
+      }).catch((error) => {
+        showModal(false)
+        console.warn(error);
+      });
+
+
     }
   }
 
