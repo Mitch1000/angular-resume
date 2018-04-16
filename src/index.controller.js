808fea231543af603a1d9a113cfdd65a0a632152
Mitch1000 <mitchell@fluidmedia.ca>
2018-04-16 10:54:20 -0400

    prevents rendering of three js script if user isn't viewing the page

--- src/index.controller.js
+++ src/index.controller.js
@@ -5,18 +5,18 @@
         .module('app', [])
         .controller('indexController', indexController);
 
-    indexController.$inject=['$scope', '$document'];
-    function indexController($scope, $document) {
-      $scope.pattern = './img/glitch1.jpg'
+    indexController.$inject=['$scope', '$document', '$window'];
+    function indexController($scope, $document , $window) {
+      this.$onInit = () => {
+        $scope.hasFocus = true;
+      }
+      
+      $window.onfocus = function(){
+        $scope.hasFocus = true;
+      }
 
-      $scope.sideBarIsOpen = false;
-
-      $scope.openSideNav = () => {
-        $scope.sideBarIsOpen = true;
-      };
-
-      $scope.goToContent = ()=> {
-        $document.scrollToElement(angular.element(document.getElementById('about')), 50, 750);
-      } 
+      $window.onblur = function(){
+        $scope.hasFocus = false;
+      }
     }
 })();
