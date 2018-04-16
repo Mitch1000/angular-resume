5bf505655a85f18cd29902cbc9343607bc7ae59e
Mitch1000 <mitchell@fluidmedia.ca>
2018-04-16 09:37:15 -0400

    adds responsive mode for about section of app

--- src/components/about/about.component.js
+++ src/components/about/about.component.js
@@ -6,7 +6,15 @@
     templateUrl: './components/about/about.template.html'
   };
 
-  aboutController.$inject = [];
-  function aboutController() {}
+  aboutController.$inject = ['$window', '$timeout'];
+  function aboutController($window, $timeout) {
+    this.$onInit = () => {
+      const isMobile = $window.innerWidth;
+      this.swapPictureWithText = false;
 
+      $timeout( () => {
+        this.swapPictureWithText = true;
+      }, 1000 );
+    } 
+  }
 }());
